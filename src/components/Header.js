215bec52b37e06ce33d49a30909496d7ad24b9ab
Andy <desperadogaga@protonmail.com>
2022-06-25 12:02:50 +0200

    Customizes portfolio

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
 // Import Assets
-import profile from '../assets/profile.png';
+import profile from '../assets/profile.jpg';
 
 const Header = () => {
     return (
         <section className='header'>
-            <img src={profile} alt="John Doe" />
+            <img src={profile} alt="Andy Torres" />
 
             <div className='header__content'>
-                <h1>Hi, I'm John Doe</h1>
+                <h1>Hi, I'm Andy Torres</h1>
                 <p>Blockchain Developer</p>
-                <button className='button'>Get In Touch</button>
+                <a href="wikipedia.org" className='button'>Get In Touch</a>
             </div>
         </section>
     );
